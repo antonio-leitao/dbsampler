c231cf5eebec4df3f3f5de437756810dc700ca8d
António Leitão <58305445+Antonio-Leitao@users.noreply.github.com>
2020-12-09 13:58:16 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Antonio-Leitao/dbsampler",
     packages=setuptools.find_packages(),
+    install_requires=['numpy','scipy','sklearn'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
