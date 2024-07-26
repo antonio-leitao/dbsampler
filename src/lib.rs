e29d5b2c6c45aee99298224ea9bc89dc0e630cf2
Antonio Leitao <aml.leitao@campus.fct.unl.pt>
2024-07-26 16:43:07 +0200

    Cleanup of delaunay code

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 use pyo3::prelude::*;
-mod delauney;
 mod linalg;
 use anyhow::{anyhow, Result};
 use hashbrown::HashSet;
@@ -234,20 +233,9 @@ fn dbs(
     Ok(cover.into_iter().map(|point| point.coords).collect())
 }
 
-//NEEDS TO ADD MORE SLACK TO GET CONVEX HULL
-#[pyfunction]
-#[pyo3(signature = (data,n_points=1000, parallel=true))]
-fn delaunay(data: Vec<Vec<f64>>, n_points: usize, parallel: bool) -> PyResult<Vec<(usize, usize)>> {
-    if parallel {
-        Ok(delauney::core_loop(data, n_points))
-    } else {
-        Ok(delauney::core_loop_parallel(data, n_points))
-    }
-}
 /// A Python module implemented in Rust.
 #[pymodule]
 fn dbsampler(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(dbs, m)?)?;
-    m.add_function(wrap_pyfunction!(delaunay, m)?)?;
     Ok(())
 }
